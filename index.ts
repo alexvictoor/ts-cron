ee8f886b98f030b7729ee5cb858c6bd74d8729b9
Alexandre VICTOOR <alexandrevictoor@MacBook-Pro-de-Admin-2.local>
2023-11-17 09:26:10 +0100

    verify function

--- index.ts
+++ index.ts
@@ -5,3 +5,7 @@ export type CRON<Value> =
   Split<Value, Space> extends [infer Minute extends string, infer Hour extends string, infer DayOfMonth  extends string, infer Month extends string, infer DayOfWeek extends string] 
     ? `${CheckMinutesPart<Minute>}${Space}${CheckHoursPart<Hour>}${Space}${CheckDaysPart<DayOfMonth>}${Space}${CheckMonthsPart<Month>}${Space}${CheckWeekDaysPart<DayOfWeek>}`
     : never;
+
+
+
+export const verifyCron = <T extends string>(cron: T & CRON<T>): T => cron;
